15fa85a8dd4687044bb0b86fae59ef1eb3e88623
Jl <2117109473@qq.com>
2019-09-13 22:09:06 +0800

    p,g,a

---
+++ protect.js
@@ -0,0 +1,47 @@
+(function () {
+    'use strict';
+    var devtools = {
+        open: false,
+        orientation: null
+    };
+    var threshold = 160;
+    var emitEvent = function (state, orientation) {
+        window.dispatchEvent(new CustomEvent('devtoolschange', {
+            detail: {
+                open: state,
+                orientation: orientation
+            }
+        }));
+    }
+    setInterval(function () {
+        var widthThreshold = window.outerWidth - window.innerWidth > threshold;
+        var heightThreshold = window.outerHeight - window.innerHeight > threshold;
+        var orientation = widthThreshold ? 'vertical' : 'horizontal';
+
+        if (!(heightThreshold && widthThreshold) &&
+            ((window.Firebug && window.Firebug.chrome && window.Firebug.chrome.isInitialized) || widthThreshold || heightThreshold)) {
+            if (!devtools.open || devtools.orientation !== orientation) {
+                emitEvent(true, orientation);
+            }
+            devtools.open = true;
+            devtools.orientation = orientation;
+        } else {
+            if (devtools.open) {
+                emitEvent(false, null);
+            }
+            devtools.open = false;
+            devtools.orientation = null;
+        }
+    }, 50)
+    if (typeof module !== 'undefined' && module.exports) {
+        module.exports = devtools;
+    } else {
+        window.devtools = devtools;
+    }
+})();
+
+
+window.addEventListener('devtoolschange', function (e) {
+    if (e.detail.open) console.clear();
+    document.write("<html><body><h1>Never F12 again!</h1><h3>If you want the code, please contact the author.</h3></body></html>");
+})
